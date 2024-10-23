5f4d53bb3c2c53742e439216cb9d1c27036438b0
Tyuiu.MyasnikovaES1@outlook.com <etichoniva12@gmail.com>
2024-10-23 22:05:01 +0500

    Initial Commit

---
+++ dx/dx_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DxAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dx_app'
---
+++ dx/dx_app/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+   
+def index(request):
+    return HttpResponse("<h2>Web сервис для развертывания и анализа моделей машинного обучения</h2>")
+  
+
+
+
+
+
